6377780bfba6b3d4400c9ea8f7e331e51e60edfc
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-12-19 21:29:15 -0300

    feat: config ntree to open on buffer

--- lua/edu-gomes/plugins/nvim-tree.lua
+++ lua/edu-gomes/plugins/nvim-tree.lua
@@ -5,15 +5,22 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-
-	config = function()
+	opts = {
+		sync_root_with_cwd = true, -- Sync the tree's root with the working directory
+		update_focused_file = { -- Highlight and center the current file in the tree
+			enable = true,
+			update_cwd = true,
+		},
+	},
+	config = function(_, opts)
 		-- Set up keymap to toggle nvim-tree
 		vim.keymap.set("n", "<Leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
+
 		-- Make the nvim-tree background transparent
 		vim.cmd("hi NvimTreeNormal guibg=NONE ctermbg=NONE") -- Transparent background for the file tree
 		vim.cmd("hi NvimTreeVertSplit guibg=NONE ctermbg=NONE") -- Transparent vertical split in file tree
 
-		require("nvim-tree").setup({
+		require("nvim-tree").setup(vim.tbl_deep_extend("force", {
 			sort = {
 				sorter = "case_sensitive",
 			},
@@ -27,6 +34,6 @@ return {
 				dotfiles = false,
 				git_ignored = false,
 			},
-		})
+		}, opts))
 	end,
 }
