1ee64f03f8a688fd15a53316e7931d3aaa74a1c8
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-12-19 22:44:46 -0300

    feat:add function to change themes

--- lua/edu-gomes/plugins/colors.lua
+++ lua/edu-gomes/plugins/colors.lua
@@ -1,56 +1,81 @@
---return {
---	-- You can easily change to a different colorscheme.
---	-- Change the name of the colorscheme plugin below, and then
---	-- change the command in the config to whatever the name of that colorscheme is.
---	--
---	-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
---	"folke/tokyonight.nvim",
---	priority = 1000, -- Make sure to load this before all the other start plugins.
---	init = function()
---		-- Load the colorscheme here.
---		-- Like many other themes, this one has different styles, and you could load
---		-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
---		vim.cmd.colorscheme("tokyonight-night")
---
---		-- You can configure highlights by doing something like:
---		vim.cmd.hi("Comment gui=none")
---	end,
---	config = function()
---		require("tokyonight").setup({
---			transparent = true,
---		})
---		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---	end,
---}
+function ChangeTheme(theme)
+	theme = theme or "tokyonight-night"
+
+	vim.cmd.colorscheme(theme)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+vim.keymap.set("n", "<Leader>t1", function()
+	ChangeTheme("tokyonight-night")
+end, { desc = "Switch to Tokyo Night" })
+vim.keymap.set("n", "<Leader>t2", function()
+	ChangeTheme("catppuccin")
+end, { desc = "Switch to Catppuccin" })
+vim.keymap.set("n", "<Leader>t3", function()
+	ChangeTheme("tokyodark")
+end, { desc = "Switch to Tokyodark" })
+
 return {
-	-- Specify the Tokyodark theme plugin.
-	"tiagovla/tokyodark.nvim",
-	priority = 1000, -- Make sure to load this before all the other start plugins.
-	opts = {
-		transparent_background = true, -- Keep the background opaque.
-		gamma = 1.00, -- Adjust the brightness level.
-		styles = {
-			comments = { italic = true }, -- Style for comments.
-			keywords = { italic = true }, -- Style for keywords.
-			identifiers = { italic = true }, -- Style for identifiers.
-			functions = {}, -- Style for functions.
-			variables = {}, -- Style for variables.
-		},
-		custom_highlights = function(highlights, palette)
-			-- Define custom highlights here.
-			return {}
+	-- TokyoNight theme setup
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("tokyonight-night")
+			vim.g.tokyonight_transparent = true
+			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.hi("Comment gui=none")
 		end,
-		custom_palette = function(palette)
-			-- Define custom palette modifications here.
-			return {}
+		config = function()
+			require("tokyonight").setup({
+				transparent = true,
+			})
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		end,
-		terminal_colors = true, -- Enable terminal colors.
 	},
-	config = function(_, opts)
-		-- Setup the Tokyodark theme with the given options.
-		require("tokyodark").setup(opts)
-		-- Apply the colorscheme.
-		vim.cmd("colorscheme tokyodark")
-	end,
+
+	-- Tokyodark theme setup
+	{
+		"tiagovla/tokyodark.nvim",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			gamma = 1.00,
+			styles = {
+				comments = { italic = true },
+				keywords = { italic = true },
+				identifiers = { italic = true },
+				functions = {},
+				variables = {},
+			},
+			custom_highlights = function(highlights, palette)
+				return {}
+			end,
+			custom_palette = function(palette)
+				return {}
+			end,
+			terminal_colors = true,
+		},
+		config = function(_, opts)
+			require("tokyodark").setup(opts)
+			vim.cmd("colorscheme tokyodark")
+		end,
+	},
+
+	-- Catppuccin theme setup
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+			})
+			ChangeTheme("catppuccin")
+		end,
+	},
 }
